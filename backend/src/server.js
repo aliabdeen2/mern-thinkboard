0a50a23988662241d6735b09309584164d4df94e
Ali Abdeen <aliashrafabdeen@hotmail.com>
2025-08-25 01:26:10 +0100

    fixed routers

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+
+const app = express();
+
+app.use("/api/notes", notesRoutes);
+
+app.listen(5002, () => {
+  console.log("Server is running on port 5002");
+});
